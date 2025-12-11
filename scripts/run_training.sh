4eb38bba197997536e61c57da2327022a7f98fe1
jstuart <jstuart@redhat.com>
2025-12-10 20:25:20 -0600

    update

--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -2,7 +2,7 @@ python3 src/train_policy.py \
     --train-path data/training/combined/stage1_train.jsonl \
     --eval-path data/training/combined/stage1_eval.jsonl \
     --output-dir models/stage1-context-inference \
-    --num-epochs 5 \
+    --num-epochs 4 \
     --learning-rate 2e-4 \
     --max-seq-len 1024 \
     --use-4bit
@@ -11,7 +11,7 @@ python3 src/train_policy.py \
     --train-path data/training/combined/stage2_train.jsonl \
     --eval-path data/training/combined/stage2_eval.jsonl \
     --output-dir models/stage2-rule-generation \
-    --num-epochs 5 \
+    --num-epochs 4 \
     --learning-rate 2e-4 \
-    --max-seq-len 4096 \
+    --max-seq-len 3072 \
     --use-4bit
